accf2afbac58db9b0bd323ff5e1331dc69a34170
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-08-26 09:28:27 +0300

    add logback config

---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,15 @@
+println "Configuring logback"
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} [%thread] %highlight(%-5level) %logger{40}: %msg%n"
+    }
+}
+
+root(INFO, ["STDOUT"])
+
+logger("org.jnaalisv.bookstore", INFO)
+logger("org.springframework", INFO)
+logger("org.hibernate", INFO)
+logger("org.hibernate.tool.hbm2ddl.SchemaExport", DEBUG)
+logger("org.hibernate.SQL", DEBUG)
